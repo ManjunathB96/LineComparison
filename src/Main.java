26a3f5ebd175b189b9d5c2390109a2f62c8ef4d5
Manju31051996 <bbelagavi6@gmail.com>
2022-10-17 20:03:15 +0530

    add main file

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        LineUC1 l1 = new LineUC1();
+        l1.p1 = new PointUC2();
+
+        l1.p1.x =10;
+        l1.p1.y =20;
+
+        l1.p2 = new PointUC2();
+
+        l1.p2.x =80;
+        l1.p2.y = 100;
+
+        LineUC1 l2 = new LineUC1();
+
+        l2.p1 = new PointUC2();
+
+        l2.p1.x  = 10;
+        l2.p1.y = 20;
+
+        l2.p2 = new PointUC2();
+
+        l2.p2.x = 80;
+        l2.p2.y = 75;
+
+
+        float length1 = l1.getLength();
+        System.out.println(length1);
+
+        float length2 = l2.getLength();
+        System.out.println(length2);
+
+        // if(length1 == length2){
+        //     System.out.println("Both lines are equal");
+
+        // }
+        // else {
+        //     System.out.println("lines are not equal");
+        // }
+
+        System.out.println(l1 == l2);
+        System.out.println(l1.equals(l2));
+
+
+
+    }
+}
