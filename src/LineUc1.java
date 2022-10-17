5a6e48426629a14e3f8f68694709bf2e805de409
Manju31051996 <bbelagavi6@gmail.com>
2022-10-17 19:48:13 +0530

    added lineUC1 file

---
+++ src/LineUc1.java
@@ -0,0 +1,10 @@
+public class LineUc1 {
+    PointUC2 p1;
+    PointUC2 p2;
+
+    float length;
+    float getLength(){
+        return (float) Math.sqrt(Math.pow((p1.x - p2.x),2 )+ Math.pow((p1.y - p2.y),2));
+
+    }
+}
